30a5baa23efb6682c7b0fe838933c2bf053c46c0
dzntech <dzn_tech@inveskey.com>
2020-11-17 23:28:59 +0530

    project directories cleaned and ready for deploy

--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,6 @@
 */
 Auth::routes();
 
-// Route::get('/', function () {
-//     return view('home');
-// });
-
 Route::get('home', 'mksHomeController@Admin_Home');
 
 Route::get('/', 'mksHomeController@index');
@@ -55,59 +51,3 @@ Route::post('document/store', 'mksHomeController@PressnoteStore')->name('pressno
 Route::get('document/{id}/edit', 'mksHomeController@PressnoteEdit')->name('pressnote.edit');
 Route::post('document/update/{id}', 'mksHomeController@PressnoteUpdate')->name('pressnote.update');
 Route::get('document/{id}/delete', 'mksHomeController@PressnoteDelete')->name('pressnote.delete');
-
-
-
-// Route::get('/upload-documents','DocumentController@createdocs')->name('docsdetails');
-// Route::post('/store/document','DocumentController@docsstore');
-
-//USER VIEW DOCUMENTS
-// Route::get('/show/document','DocumentController@docsshow');
-// Route::get('/edit/{id}','DocumentController@editdocs');
-// Route::put('/updatedocs/{id}','DocumentController@updatedocs');
-// Route::get('/document/delete/{id}','DocumentController@docsdestroy');
-
- //CMS VIEW BLOGS
-//  Route::get('/contentupload','BlogController@cms')->name('writeblog');
-//  Route::post('/contentupload/blogstore','BlogController@blogstore');
-
- //user views
-//  Route::get('/blogs/show','BlogController@blogshow');
-//  Route::get('/edit/blog/{id}','BlogController@edit');
-//  Route::get('/blog/delete/{id}','BlogController@blogdestroy');
-//  Route::put('/updateblog/{id}','BlogController@update');
-
- //CMS VIEW VIDEOS
-//  Route::get('/videodetails','VideosController@newvideo')->name('videocms');
-//  Route::post('/video/videostore','VideosController@videostore');
-
- //USER VIEWS VIDEO
-//  Route::get('/videos/show','VideosController@showvideos');
-//  Route::get('/single-video/{id}','VideosController@singlevideo');
-//  Route::get('/video/edit/{id}','VideosController@videoedit');
-//  Route::put('/video/update/{id}','VideosController@update');
-//  Route::get('/delete/{id}','VideosController@destroy');
- 
- //CMS VIEW AUDIO
-//  Route::get('/audio','AudioController@audioindex')->name('uploadaudio');
-//  Route::post('/audio/audiostore','AudioController@audiostore');
-
- //USER VIEW AUDIO
-//  Route::get('/audios/show','AudioController@audioshow');
-//  Route::get('/edit/audio/{id}','AudioController@editaudio');
-//  Route::put('/update/audio/{id}','AudioController@updateaudio');
-//  Route::get('/audio/delete/{id}','AudioController@destroyaudio');
-//  Route::get('/single-audio/{id}','AudioController@singleaudio');
-
-//  //cms view presentations
-//  Route::get('/upload-presentations','PresentationController@docsindex')->name('pptdetails');
-//  Route::post('/store/presentation','PresentationController@storeppt');
-
-//  //USER VIEW PRESENTATION
-//  Route::get('/show/presentations','PresentationController@showppt');
-//  Route::get('/edit/ppt/{id}','PresentationController@editppt');
-//  Route::put('/updateppt/{id}','PresentationController@updateppt');
-//  Route::get('/ppt/delete/{id}','PresentationController@pptdestroy');
-
-
- 
