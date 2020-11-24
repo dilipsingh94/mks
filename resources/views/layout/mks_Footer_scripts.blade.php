3c34a343f6aa257134138a21cda70b21ace2b829
dzntech <dzn_tech@inveskey.com>
2020-11-24 21:13:11 +0530

    disabled right click operations on webpage

--- resources/views/layout/mks_Footer_scripts.blade.php
+++ resources/views/layout/mks_Footer_scripts.blade.php
@@ -3,5 +3,19 @@
       <script src="{{ mix('js/mks_scripts.js') }}"></script>
 
       <script src="https://unpkg.com/bootstrap-table@1.18.0/dist/bootstrap-table.min.js"></script>
+
+      <script type="text/javascript">
+        $(document).ready(function () {
+            //Disable cut copy paste
+            $('body').bind('cut copy paste', function (e) {
+                e.preventDefault();
+            });
+          
+            //Disable mouse right click
+            $("body").on("contextmenu",function(e){
+                return false;
+            });
+        });
+      </script>
   </body>
 </html>
